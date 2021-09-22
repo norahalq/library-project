0d26681725ebe1e5c66e4aa8b70ec94126c86a28
Norah <84841948+norahalq@users.noreply.github.com>
2021-09-22 23:53:36 +0300

    first_edit
    Html&&CSS&&js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//Your task is to make a function that can take any non-negative integer as 
+//an argument and return it with its digits in descending order. Essentially, 
+//rearrange the digits to create the highest possible number.
+
+//مهمتك هي إنشاء دالة يمكن أن تأخذ أي عدد صحيح غير سالب كوسيطة وإعادتها بأرقامها بترتيب تنازلي.
+// بشكل أساسي ، أعد ترتيب الأرقام لإنشاء أكبر رقم ممكن.
+//Input: 42145 Output: 54421
+// function descendingOrder(n){
+//     return parseInt(String(n).split('').sort().reverse().join(''))
+//   }
+
+
